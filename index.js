86d9b92fd8f837e19ad995601f3ade5c8acc3c50
nya1 <nya1@protonmail.com>
2019-04-23 13:06:12 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+const prompts = require('prompts')
+const path = require('path')
+const fs = require('fs')
+const ejs = require('ejs')
+const copy = require('recursive-copy')
+const debug = require('debug')('m10-cli')
+
+// args
+const argv = require('yargs')
+  .command('new <dir>', 'Create a new m10 based project')
+  .help()
+  .demandCommand(1).argv
+
+// return pretty error without stacktrace
+function cleanError (msg) {
+  console.error('Error: ' + msg)
+  return process.exit(1)
+}
+
+async function main () {
+  function onCancel (prompt, response) {
+    console.log('Exiting...')
+    process.exit(0)
+  }
+  if (argv.dir && argv._.indexOf('new') > -1) {
+    let pathDir = path.resolve(argv.dir)
+    let dirExists = true
+    try {
+      fs.lstatSync(pathDir).isDirectory()
+      dirExists = true
+    } catch (e) {
+      dirExists = false
+    }
+    if (dirExists === true) {
+      return cleanError(
+        `Directory ${pathDir} already exists, please delete it and try again or choose a different one`
+      )
+    }
+    let cleanRegex = /[^\w.\-\_]+/g
+    let cleanProjectName = argv.dir.replace(cleanRegex, '-')
+    console.log(`> Folder: ${pathDir}`)
+
+    let setup = await prompts(
+      [
+        {
+          type: 'text',
+          name: 'projectName',
+          message: 'Project name',
+          initial: cleanProjectName,
+          validate: value =>
+            value.match(cleanRegex) === null
+              ? true
+              : 'Only alphanumeric . - _ allowed'
+        },
+        {
+          type: 'confirm',
+          name: 'mongodb',
+          message: 'Add mongodb support?'
+        }
+      ],
+      { onCancel }
+    )
+
+    // copy
+    await copy('./templates/starter', pathDir)
+    // change custom files with options
+    let filesToCompile = ['package.json', 'app.js']
+    for (let i = 0; i < filesToCompile.length; i++) {
+      let f = filesToCompile[i]
+      let p = `${pathDir}/${f}`
+      let r = await ejs.renderFile(p, setup)
+      fs.writeFileSync(p, r)
+    }
+    console.log('> Done!\nNow run:')
+    console.log(`cd ${pathDir} && npm install`)
+  }
+}
+
+main()
---
+++ templates/starter/app.js
@@ -0,0 +1,28 @@
+const m10 = require('m10')
+const express = require('express')
+const bodyparser = require('body-parser')
+const morgan = require('morgan')
+<%_ if (mongodb) { _%>
+const expressMongoDb = require('express-mongo-db')
+<%_ } _%>
+const helmet = require('helmet')
+const app = express()
+const port = process.env.PORT || 3000
+// load config
+const config = require('./config.json')
+// load basic middlewares
+app.use(helmet())
+app.use(bodyparser.json())
+app.use(morgan('tiny'))
+<%_ if (mongodb) { _%>
+// mongodb
+app.use(expressMongoDb('mongodb://localhost/test'));
+<%_ } _%>
+// load config into this app
+m10.init(config, app)
+
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, () => console.log(`> Listening on localhost:${port} on ${process.env.NODE_ENV} mode`))
+}
+
+module.exports = app
